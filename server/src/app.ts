e80232833ee81f5a2a5c607a91e7949e69daa229
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2024-12-31 01:32:10 +0545

    chore: setup logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,18 @@
 import express, { Express } from "express";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+import logger from "./config/logger";
+
 const app: Express = express();
 
-// all middlewares
+// middlewares
 
-// all routers
+// routers
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello world" });
+});
 
 export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 import app from "./app";
 
-app.get("/", (req, res) => {
-  res.json({ message: "hello world" });
-});
+// database config
+
+// other services configurations error handling related to them
 
 app.listen(8000, () => {
   console.log("Server is running on http://localhost:8000");
