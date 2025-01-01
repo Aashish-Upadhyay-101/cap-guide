033d78d98e1ba1bc77e0a80ad257d21e732a1a10
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2025-01-01 11:51:55 +0545

    chore: winston logger steup

--- server/src/config/logger.ts
+++ server/src/config/logger.ts
@@ -1,9 +1,30 @@
 import winston from "winston";
 
+const logLevels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6,
+};
+
+const { timestamp, json, cli } = winston.format;
+
 const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || "info",
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+  level: "info",
+  levels: logLevels,
+  format: timestamp(),
+  transports: [
+    new winston.transports.Console({
+      format: cli({ all: true }),
+    }),
+    new winston.transports.File({
+      filename: "app.log",
+      format: json(),
+    }),
+  ],
 });
 
 export default logger;
