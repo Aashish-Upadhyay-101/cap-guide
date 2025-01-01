64741b2322d1c3691ea0b98c48c68af699ad1a9f
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2025-01-02 02:18:32 +0545

    refactor: removed unwanted types

--- server/src/middlewares/httpContext.ts
+++ server/src/middlewares/httpContext.ts
@@ -1,21 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import { asyncLocalStorage } from "../config/logger";
 
-type HttpContext = {
-  method: string;
-  url: string;
-  ip: string | undefined;
-  userAgent: string | undefined;
-  status?: number | undefined;
-  responseTime?: number;
-};
-
 export function httpContextMiddleware(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-  const httpContext: HttpContext = {
+  const httpContext = {
     method: req.method,
     url: req.originalUrl,
     ip: req.ip,
