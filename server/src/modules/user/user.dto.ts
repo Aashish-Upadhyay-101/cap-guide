4c1fcb581f63135b74f065364c2dfa4bce47ceec
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2025-01-09 15:21:42 +0545

    feat: login and register user dto

---
+++ server/src/modules/user/user.dto.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const ZLoginUserSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be atleast 6 characters long"),
+});
+
+export const ZRegisterUserSchema = z
+  .object({
+    firstName: z.string().max(35).nonempty(),
+    lastName: z.string().max(35).nonempty(),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be atleast 6 characters long"),
+    passwordConfirm: z.string(),
+  })
+  .refine((val) => val.password == val.passwordConfirm, {
+    message: "Password and confirm password doesn't match",
+  });
+
+export type LoginUserDTO = z.infer<typeof ZLoginUserSchema>;
+export type RegisterUserDTO = z.infer<typeof ZRegisterUserSchema>;
