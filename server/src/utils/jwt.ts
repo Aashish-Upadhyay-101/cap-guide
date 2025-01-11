dcb3ca836b670e52f5835f19ad4ed90e9b7c5639
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2025-01-11 12:16:22 +0545

    feat: jwt class skeleton

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,22 @@
+import { JwtPayload, SignOptions } from "jsonwebtoken";
+
+class JWT {
+  private readonly secretKey: string;
+  private readonly expiresIn: string;
+  private readonly issuer: string;
+
+  constructor(secretKey: string, expiresIn: string, issuer: string) {
+    this.secretKey = secretKey;
+    this.expiresIn = expiresIn;
+    this.issuer = issuer;
+  }
+
+  public generateToken(
+    payload: Record<string, unknown>,
+    options?: SignOptions,
+  ): string {}
+
+  public verifyToken<T extends JwtPayload>(token: string): T {}
+}
+
+export default JWT;
