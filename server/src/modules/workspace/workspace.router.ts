28545996255a458b5b333fd984746e33b7327ddf
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2025-01-17 15:21:26 +0545

    feat: workspace router setup

--- server/src/modules/workspace/workspace.router.ts
+++ server/src/modules/workspace/workspace.router.ts
@@ -1,10 +1,28 @@
 import BaseRouter from "../core/base.router";
 
 class WorkspaceRouter extends BaseRouter {
-  constructor() {
+  private workspaceController: WorkspaceController;
+
+  constructor(workspaceController: WorkspaceController) {
     super();
 
-    this.setRoutes((router) => {});
+    this.workspaceController = workspaceController;
+
+    this.setRoutes((router) => {
+      router.get("/:id", this.workspaceController.getWorkspace);
+      router.post("/:id", this.workspaceController.createWorkspace);
+      router.patch("/:id", this.workspaceController.updateWorkspace);
+      router.delete("/:id", this.workspaceController.deleteWorkspace);
+      router.post("/:id/member", this.workspaceController.addWorkspaceMember);
+      router.delete(
+        "/:id/member/:memberId",
+        this.workspaceController.removeWorkspaceMember,
+      );
+      router.patch(
+        "/:id/member/:memberId",
+        this.workspaceController.updateWorkspaceMemberRole,
+      );
+    });
   }
 }
 
