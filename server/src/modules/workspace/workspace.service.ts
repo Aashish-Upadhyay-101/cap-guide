a281516064092e40bc32c445d1e39a148395b0a9
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2025-01-17 15:08:14 +0545

    feat: workspace service setup

---
+++ server/src/modules/workspace/workspace.service.ts
@@ -0,0 +1,15 @@
+class WorkspaceService {
+  public createWorkspace() {}
+
+  public getWorkspace() {}
+
+  public updateWorkspace() {}
+
+  public deleteWorkspace() {}
+
+  public addWorkspaceMember() {}
+
+  public removeWorkspaceMember() {}
+
+  public updateWorkspaceMemberRole() {}
+}
